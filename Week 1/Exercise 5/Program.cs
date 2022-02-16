1a717b63b8478fdbafc13999c4d0e410686470d7
Can İçcan <95756970+caniccan@users.noreply.github.com>
2022-02-16 17:44:14 +0300

    Add files via upload

---
+++ Week 1/Exercise 5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _5_Alıştırma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a =0;
+            Console.Write("Bir sayı giriniz: ");
+            a= int.Parse(Console.ReadLine());
+
+            if (a <= 100 && 0 <= a)
+            {
+                Console.WriteLine($"{a} Sayısı 0-100 sayıları arasındadır.");
+            }
+            else
+            {
+                Console.WriteLine($"{a} Sayısı 0-100 sayıları arasında değildir.");
+            }
+
+        }
+    }
+}
