1b369a97f323ddd3388d05284d0725de8b3d1bb3
Can İçcan <95756970+caniccan@users.noreply.github.com>
2022-02-28 19:52:29 +0300

    Add files via upload

---
+++ Week 2/Exercise 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayac =0;
+            Console.WriteLine("N sayısını giriniz: ");
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                if (i % 3 == 0 && i% 7 == 0)
+                {
+                    Console.WriteLine(i);
+                    sayac++;
+                }
+            }
+            Console.WriteLine("========================");
+            Console.WriteLine($"1-{N} değer aralığında "+sayac+" adet 3'e ve 7'ye tam bölünen sayı vardır.");
+        }
+    }
+}
