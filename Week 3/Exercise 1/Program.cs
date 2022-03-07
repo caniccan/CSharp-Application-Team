ce0fd90f5ffd8af835eb85f46d9cad68f9477781
Can İçcan <95756970+caniccan@users.noreply.github.com>
2022-03-07 22:30:53 +0300

    Add files via upload

---
+++ Week 3/Exercise 1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir metin giriniz : ");
+            string a = Console.ReadLine().Trim();
+            string[] b = a.Split(" ");
+            Console.WriteLine("Girdiğiniz metinde {0} kelime bulunmaktadır.",b.Length);
+        }
+    }
+}
