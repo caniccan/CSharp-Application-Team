9fb6140dba9d4bf9126d7f5c47bd9700f42f2158
Can İçcan <95756970+caniccan@users.noreply.github.com>
2022-03-07 22:33:27 +0300

    Add files via upload

---
+++ Week 3/Exercise 8/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Exercise_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Cümle giriniz : ");
+            string[] a = Console.ReadLine().Split(" ");
+            Console.WriteLine(a[0]);
+        }
+    }
+}
